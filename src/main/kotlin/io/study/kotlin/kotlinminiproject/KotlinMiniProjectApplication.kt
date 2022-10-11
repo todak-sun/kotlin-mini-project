b85bd07c02ff809857170f24b0f14ea829f534c6
todak-sun <tjsdydwn@gmail.com>
2022-10-11 19:18:06 +0900

    사다리게임 요구사항 정리

---
+++ src/main/kotlin/io/study/kotlin/kotlinminiproject/KotlinMiniProjectApplication.kt
@@ -0,0 +1,11 @@
+package io.study.kotlin.kotlinminiproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMiniProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinMiniProjectApplication>(*args)
+}
---
+++ src/test/kotlin/io/study/kotlin/kotlinminiproject/KotlinMiniProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.study.kotlin.kotlinminiproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMiniProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
