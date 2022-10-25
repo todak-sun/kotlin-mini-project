3b25476efbe0dab427809ce433ad8f2b5e9cde84
Lullaby <sds901234@naver.com>
2022-10-26 07:53:57 +0900

    feat: 개발중

--- src/main/kotlin/io/study/kotlin/kotlinminiproject/domain/LadderGame.kt
+++ src/main/kotlin/io/study/kotlin/kotlinminiproject/domain/LadderGame.kt
@@ -1,33 +1,57 @@
 package io.study.kotlin.kotlinminiproject.domain
 
 import java.util.UUID
+import java.util.stream.IntStream
+import kotlin.streams.toList
 
 class LadderGame {
 
     val id: UUID = UUID.randomUUID()
-
-
+    val request: LadderGameStartRequest
+    val legs: MutableList<Leg>
+    val destinations: MutableList<Destination>
 
     constructor(request: LadderGameStartRequest) {
+        this.request = request
+        this.legs = mutableListOf()
+        this.destinations = mutableListOf()
+        validate()
+        initialize()
 
-        if (request.participants.isEmpty() || request.participants.size > 100) {
-            throw Error("참가자는 최소 2명, 최대 100명까지 가능합니다. [입력값 = $request.participants.size]")
+        for (order: Int in 1..this.request.participants.size) {
+            this.legs.add(Leg(order))
         }
-
     }
 
     fun validate() {
+        if (request.participants.size !in 2..100) {
+            throw Error("참가자는 최소 2명, 최대 100명까지 가능합니다. [입력값 = $request.participants.size]")
+        }
 
+        if (request.rung != null && request.rung !in 3..20) {
+            throw Error("Rung은 3이상 20이하로 입력 해주세요. [입력값 = $request.rung]")
+        }
+
+        if (request.destinations.size > request.participants.size) {
+            throw Error("목적지는 참가자 수 보다 많이 입력할 수 없습니다. [입력값 = $request.destinations.size]")
+        }
     }
 
-    fun start(request: LadderGameStartRequest): List<LadderGameResult> {
+    fun initialize() {
+        if (request.destinations.size < request.participants.size) {
+            val difference = request.participants.size - request.destinations.size
+            for (number: Int in 1 .. difference) {
+                this.destinations.add(Destination.none())
+            }
+        }
+    }
 
-
+    fun start(): List<LadderGameResult> {
         println("Ladder Game is started.")
 
-
-        return request.participants
-            .mapIndexed { index, it ->  LadderGameResult(Participant(index + 1, it), Destination("test"), listOf(1))}
+        return listOf()
+//        return request.participants
+//            .mapIndexed { index, it ->  LadderGameResult(Participant(index + 1, it), Destination("test"), listOf(1))}
     }
 
 }
@@ -38,20 +62,29 @@ data class LadderGameStartRequest(
     val rung: Int?
 )
 
-data class Participant(val order: Int, val name: String)
-fun Participant.ghost(order: Int): Participant = Participant(order, "GHOST")
+data class Participant(val order: Int, val name: String) {
+    companion object Factory {
+        fun ghost(order: Int): Participant = Participant(order, "GHOST")
+    }
+}
 
-data class Destination(val name: String)
-fun Destination.none(): Destination = Destination("NONE")
+data class Destination(val name: String){
+    companion object Factory {
+        fun none(): Destination = Destination("NONE")
+    }
+}
 
 data class LadderGameResult(
     val participant: Participant,
-    val destination : Destination, // 목적지(결과)의 이름
-    val routes : List<Int>, // 참여자가 목적지까지 간 경로(Leg의 번호)
+    val destination: Destination, // 목적지(결과)의 이름
+    val routes: List<Int>, // 참여자가 목적지까지 간 경로(Leg의 번호)
 )
 
 class Leg {
-
+    val order: Int
+    constructor(order: Int) {
+        this.order = order
+    }
 }
 
 class Rung {
--- src/test/kotlin/io/study/kotlin/kotlinminiproject/domain/LadderGameTest.kt
+++ src/test/kotlin/io/study/kotlin/kotlinminiproject/domain/LadderGameTest.kt
@@ -1,6 +1,5 @@
 package io.study.kotlin.kotlinminiproject.domain
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -8,28 +7,29 @@ import java.util.*
 
 internal class LadderGameTest {
 
+    @Test
     internal fun practice() {
-        val first = Participant(1, "Apple")
-        val second = Participant(1, "Apple")
-        assertTrue(first == second)
+        val number: Int?
+
+
     }
 
-    @DisplayName("[정상] [1] 사다리 게임이 정상적으로 생성된다. (참가자 = 4명, 목적지 = 4개)")
-    @Test
+    //@DisplayName("[정상] [1] 사다리 게임이 정상적으로 생성된다. (참가자 = 4명, 목적지 = 4개)")
+    //@Test
     internal fun pass_1() {
-//        val request = LadderGameStartRequest(
-//            participants = listOf("Ashe", "Akali", "Katarina", "Caitlyn"),
-//            destinations = listOf("Apple", "Banana", "Citron", "Durian"),
-//            null
-//        )
-//        val ladderGame = LadderGame()
-//        val ladderGameResults = ladderGame.start(request)
-//        assertNotNull(ladderGameResults)
-//
-//        assertGameResult(ladderGameResults.find { it.participant == "Ashe" }, 1, "Ashe")
-//        assertGameResult(ladderGameResults.find { it.participant == "Akali" }, 2, "Akali")
-//        assertGameResult(ladderGameResults.find { it.participant == "Katarina" }, 3, "Katarina")
-//        assertGameResult(ladderGameResults.find { it.participant == "Caitlyn" }, 4, "Caitlyn")
+        val request = LadderGameStartRequest(
+            participants = listOf("Ashe", "Akali", "Katarina", "Caitlyn"),
+            destinations = listOf("Apple", "Banana", "Citron", "Durian"),
+            null
+        )
+        val ladderGame = LadderGame(request)
+        val ladderGameResults = ladderGame.start()
+        assertNotNull(ladderGameResults)
+
+        assertGameResult(ladderGameResults.find { it.participant.name == "Ashe" }, 1, "Ashe")
+        assertGameResult(ladderGameResults.find { it.participant.name == "Akali" }, 2, "Akali")
+        assertGameResult(ladderGameResults.find { it.participant.name == "Katarina" }, 3, "Katarina")
+        assertGameResult(ladderGameResults.find { it.participant.name == "Caitlyn" }, 4, "Caitlyn")
     }
 
     @DisplayName("[정상] [1] 사다리 게임이 정상적으로 생성된다. (참가자 = 4명, 목적지 = 4개, 단계 = 8)")
@@ -43,9 +43,54 @@ internal class LadderGameTest {
 
     }
 
-//    @DisplayName("[실패] [1]사다리 게임 인원이 2명 보다 적으면 게임을 시작할 수 없다.")
-//    @DisplayName("[실패] [2]사다리 게임 인원이 100명 보다 많으면 게임을 시작할 수 없다.")
-//    @DisplayName("[실패] [3]사다리 게임 목적지가 참가자보다 많으면 게임을 시작할 수 없다.")
+    @DisplayName("[실패] [1]사다리 게임 인원이 2명 보다 적으면 게임을 시작할 수 없다.")
+    @Test
+    internal fun fail_1() {
+        val exception = assertThrows(Error::class.java) {
+            val request = LadderGameStartRequest(
+                participants = listOf("Ashe"),
+                destinations = listOf("Apple"),
+                null
+            )
+            LadderGame(request)
+        }
+        assertTrue(exception.message!!.contains("참가자는 최소 2명, 최대 100명까지 가능합니다."))
+    }
+
+    @DisplayName("[실패] [2]사다리 게임 인원이 100명 보다 많으면 게임을 시작할 수 없다.")
+    @Test
+    internal fun fail_2() {
+        val exception = assertThrows(Error::class.java) {
+            val participants: MutableList<String> = mutableListOf()
+            val destinations: MutableList<String> = mutableListOf()
+            for (number: Int in 1..200) {
+                participants.add("participant_$number")
+                destinations.add("destination_$number")
+            }
+
+            val request = LadderGameStartRequest(
+                participants = participants,
+                destinations = destinations,
+                null
+            )
+            LadderGame(request)
+        }
+        assertTrue(exception.message!!.contains("참가자는 최소 2명, 최대 100명까지 가능합니다."))
+    }
+
+    @DisplayName("[실패] [3]사다리 게임 목적지가 참가자보다 많으면 게임을 시작할 수 없다.")
+    @Test
+    internal fun fail_3() {
+        val exception = assertThrows(Error::class.java) {
+            val request = LadderGameStartRequest(
+                participants = listOf("Ashe", "Akali", "Katarina"),
+                destinations = listOf("Apple", "Banana", "Citron", "Durian"),
+                null
+            )
+            LadderGame(request)
+        }
+        assertTrue(exception.message!!.contains("목적지는 참가자 수 보다 많이 입력할 수 없습니다."))
+    }
 
     fun assertGameResult(result: LadderGameResult?, order: Int, participant: String) {
         assertNotNull(result)
